5ade9e3e99b73752802a4591b9bb600b4409b74c
Andrew Cuthbert <acuthbert@gcdtech.com>
2016-05-24 11:57:54 +0100

    Change to support binding logic move

--- src/Leaves/ModelBoundModel.php
+++ src/Leaves/ModelBoundModel.php
@@ -17,4 +17,52 @@ class ModelBoundModel extends LeafModel
      * @var Collection
      */
     public $restCollection;
+
+    public function getBoundValue($propertyName, $index = null)
+    {
+        if ($this->restModel){
+            $columns = $this->restModel->getSchema()->getColumns();
+            if (!isset($columns[$propertyName])){
+                return parent::getBoundValue($propertyName, $index);
+            }
+        } else {
+            return parent::getBoundValue($propertyName, $index);
+        }
+
+        if ($index !== null ){
+            if (isset($this->restModel->$propertyName[$index])){
+                return $this->restModel->$propertyName[$index];
+            } else {
+                return null;
+            }
+        } else {
+            return isset($this->restModel->$propertyName) ? $this->restModel->$propertyName : null;
+        }
+    }
+
+    public function setBoundValue($propertyName, $value, $index = null)
+    {
+        if ($this->restModel){
+            $columns = $this->restModel->getSchema()->getColumns();
+            if (!isset($columns[$propertyName])){
+                parent::setBoundValue($propertyName, $value, $index);
+                return;
+            }
+        } else {
+            parent::setBoundValue($propertyName, $value, $index);
+            return;
+        }
+
+        if ($index !== null){
+            if (!isset($this->restModel->$propertyName) || !is_array($this->restModel->$propertyName)){
+                $this->restModel->$propertyName = [];
+            }
+
+            $this->restModel->$propertyName[$index] = $value;
+        } else {
+            $this->restModel->$propertyName = $value;
+        }
+    }
+
+
 }
